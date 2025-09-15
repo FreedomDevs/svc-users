6bbb59a0523e705fdc7035c9a1acd5e10283527d
Foksif <knenrerp@gmail.com>
2025-09-15 12:48:25 +0700

    create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '@prisma/prisma.service';
+import { CreateUserDto } from './dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
+
+  create(createUserDto: CreateUserDto) {
+    if (!createUserDto.name || !createUserDto.password) {
+      throw new BadRequestException({
+        success: false,
+        message: 'Name and password are required.',
+      });
+    }
+    const user = this.prisma.user.create({
+      data: {
+        name: createUserDto.name,
+        password: createUserDto.password,
+        roles: ['USER'],
+      },
+    });
+
+    return {
+      success: true,
+      message: 'User created successfully',
+      data: user,
+    };
+  }
 }

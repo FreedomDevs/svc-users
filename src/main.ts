7df227e559136a734f01cf3b05c74de176291d91
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-09-20 16:43:11 +0300

    Чета подабавлял

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 9002);
 }
 bootstrap();

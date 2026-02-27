a374877dce29b1c76d810cdf7aed62d4e3bb6aff
Foksif <knenrerp@gmail.com>
2026-02-27 21:09:26 +0700

    упс 2

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -8,7 +8,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     await this.$connect();
   }
 
-  async enableShutdownHooks(app: INestApplication) {
+  enableShutdownHooks(app: INestApplication) {
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
     process.on('beforeExit', async () => {
       await app.close();

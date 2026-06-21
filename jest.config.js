e16fd7c010ecddbc3175ee86583efc2319868b68
Foksif <knenrerp@gmail.com>
2026-06-21 23:45:21 +0700

    чёто пофиксел

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,25 @@
-// jest.config.js
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  rootDir: 'src',
+  rootDir: '.',
   testRegex: '.*\\.spec\\.ts$',
   moduleFileExtensions: ['ts', 'js', 'json'],
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
+  collectCoverageFrom: ['src/**/*.(t|j)s'],
+  coverageDirectory: 'coverage',
   moduleNameMapper: {
-    // Локальные файлы проекта, исключая @prisma/client
-    '^@prisma/(?!client)(.+)$': '<rootDir>/../src/prisma/$1',
-    '^@common/(.+)$': '<rootDir>/../src/common/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@common/(.*)$': '<rootDir>/src/common/$1',
+    '^@prisma/prisma.service$': '<rootDir>/src/infra/prisma/prisma.service',
   },
+
   transform: {
     '^.+\\.(t|j)s$': [
       'ts-jest',
       {
-        tsconfig: '<rootDir>/../tsconfig.json',
+        tsconfig: '<rootDir>/tsconfig.json',
       },
     ],
   },
+
   transformIgnorePatterns: ['node_modules/(?!@prisma/client)'],
 };
